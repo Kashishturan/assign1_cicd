f6513269fa9a028e539e1e67225e4f6dfa78b79e
PrinceV007 <143865246+PrinceV007@users.noreply.github.com>
2025-01-21 11:50:25 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,4 +1,4 @@
-public class StaticIntCalculator {
+public class hello {
     public static void main(String[] args) {
         int num1 = 10, num2 = 5;
         char operator = '+';  // Change this to '+', '-', '*', or '/'
